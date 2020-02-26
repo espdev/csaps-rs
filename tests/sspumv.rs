d36fae2b59a8019506bd71b6f7d2621ff5f0e936
Eugene Prilepin <esp.home@gmail.com>
2020-02-27 02:09:27 +0300

    refactoring tests. add tests for make nd 4pt

--- tests/sspumv.rs
+++ tests/sspumv.rs
@@ -1,4 +1,4 @@
-use ndarray::{array};
+use ndarray::{array, Array, Array2, Dimension, Array1, NdFloat};
 use csaps::CubicSmoothingSpline;
 
 
@@ -20,64 +20,90 @@ fn test_new() {
     assert_eq!(spline3.ndim(), y3.ndim());
 }
 
-#[test]
-fn test_make_1d_2pt() {
-    let x = array![1., 2.];
-    let y = array![1., 2.];
-
+fn test_driver_make_nd_npt<T, D>(x: Array1<T>, y: Array<T, D>,
+                                 order: u32, pieces: u32, coeffs: Array2<T>)
+    where T: NdFloat, D: Dimension
+{
     let spline = CubicSmoothingSpline::new(&x, &y)
         .make()
         .unwrap();
 
     assert!(spline.is_valid());
-    assert_eq!(spline.order().unwrap(), 2);
-    assert_eq!(spline.pieces().unwrap(), 1);
-    assert_eq!(spline.coeffs().unwrap(), array![[1., 1.]]);
+    assert_eq!(spline.order().unwrap(), order);
+    assert_eq!(spline.pieces().unwrap(), pieces);
+    assert_eq!(spline.coeffs().unwrap(), coeffs);
+}
+
+fn test_driver_make_nd_2pt<T, D>(x: Array1<T>, y: Array<T, D>, coeffs: Array2<T>)
+    where T: NdFloat, D: Dimension
+{
+    test_driver_make_nd_npt(x, y, 2, 1, coeffs);
+}
+
+fn test_driver_make_nd_4pt<T, D>(x: Array1<T>, y: Array<T, D>, coeffs: Array2<T>)
+    where T: NdFloat, D: Dimension
+{
+    test_driver_make_nd_npt(x, y, 4, 3, coeffs);
+}
+
+#[test]
+fn test_make_1d_2pt() {
+    test_driver_make_nd_2pt(
+        array![1., 2.],
+        array![1., 2.],
+        array![[1., 1.]]
+    );
 }
 
 #[test]
 fn test_make_2d_2pt() {
-    let x = array![1., 2.];
-    let y = array![[1., 2.], [3., 5.]];
-
-    let spline = CubicSmoothingSpline::new(&x, &y)
-        .make()
-        .unwrap();
-
-    assert!(spline.is_valid());
-    assert_eq!(spline.order().unwrap(), 2);
-    assert_eq!(spline.pieces().unwrap(), 1);
-    assert_eq!(spline.coeffs().unwrap(), array![[1., 1.], [2., 3.]]);
+    test_driver_make_nd_2pt(
+        array![1., 2.],
+        array![[1., 2.], [3., 5.]],
+        array![[1., 1.], [2., 3.]]
+    );
 }
 
 #[test]
 fn test_make_3d_2pt() {
-    let x = array![1., 2.];
-    let y = array![[[1., 2.], [3., 5.]], [[2., 4.], [4., 7.]]];
-
-    let spline = CubicSmoothingSpline::new(&x, &y)
-        .make()
-        .unwrap();
-
-    assert!(spline.is_valid());
-    assert_eq!(spline.order().unwrap(), 2);
-    assert_eq!(spline.pieces().unwrap(), 1);
-    assert_eq!(spline.coeffs().unwrap(), array![[1., 1.], [2., 3.], [2., 2.], [3., 4.]]);
+    test_driver_make_nd_2pt(
+        array![1., 2.],
+        array![[[1., 2.], [3., 5.]], [[2., 4.], [4., 7.]]],
+        array![[1., 1.], [2., 3.], [2., 2.], [3., 4.]]
+    );
 }
 
 #[test]
-fn test_make() {
-    let x = array![1., 2., 3., 4.];
-    let y = array![1., 2., 3., 4.];
+fn test_make_1d_4pt() {
+    test_driver_make_nd_4pt(
+        array![1., 2., 3., 4.],
+        array![1., 2., 3., 4.],
+        array![[0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 2., 3.]]
+    );
+}
 
-    let spline = CubicSmoothingSpline::new(&x, &y)
-        .make()
-        .unwrap();
+#[test]
+fn test_make_2d_4pt() {
+    test_driver_make_nd_4pt(
+        array![1., 2., 3., 4.],
+        array![[1., 2., 3., 4.], [1., 3., 5., 7.]],
+        array![
+            [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 2., 3.],
+            [0., 0., 0., 0., 0., 0., 2., 2., 2., 1., 3., 5.]]
+    );
+}
 
-    assert!(spline.is_valid());
-    assert!(spline.order().is_some());
-    assert!(spline.pieces().is_some());
-    assert!(spline.coeffs().is_some());
+#[test]
+fn test_make_3d_4pt() {
+    test_driver_make_nd_4pt(
+        array![1., 2., 3., 4.],
+        array![[[1., 2., 3., 4.], [1., 3., 5., 7.]], [[2., 4., 6., 8.], [3., 4., 5., 6.]]],
+        array![
+            [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 2., 3.],
+            [0., 0., 0., 0., 0., 0., 2., 2., 2., 1., 3., 5.],
+            [0., 0., 0., 0., 0., 0., 2., 2., 2., 2., 4., 6.],
+            [0., 0., 0., 0., 0., 0., 1., 1., 1., 3., 4., 5.]]
+    );
 }
 
 #[test]
@@ -93,4 +119,3 @@ fn test_evaluate() {
 
     assert_eq!(ys, y);
 }
-
