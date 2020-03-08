00a658f7959389a858e38093471e7d588e4e4d11
Eugene Prilepin <esp.home@gmail.com>
2020-03-09 00:11:27 +0300

    use static dimensionality instead of dynamic for result data array

--- src/sspumv/evaluate_spline.rs
+++ src/sspumv/evaluate_spline.rs
@@ -17,11 +17,17 @@ impl<'a, T, D> CubicSmoothingSpline<'a, T, D>
 {
     pub(crate) fn evaluate_spline(&self, xi: ArrayView1<'a, T>) -> Result<Array<T, D>> {
         let axis = self.axis.unwrap();
-        let mut shape = self.y.shape().to_owned();
-        shape[axis.0] = xi.len();
+        let mut shape_t = self.y.shape().to_owned();
+        shape_t[axis.0] = xi.len();
+
+        let mut shape = D::zeros(self.y.ndim());
+
+        for (i, &s) in shape_t.iter().enumerate() {
+            shape[i] = s
+        }
 
         let yi_2d = self.spline.as_ref().unwrap().evaluate(xi);
-        let yi = ndarrayext::from_2d(&yi_2d, &shape, axis)?.to_owned();
+        let yi = ndarrayext::from_2d(&yi_2d, shape, axis)?.to_owned();
 
         Ok(yi)
     }
