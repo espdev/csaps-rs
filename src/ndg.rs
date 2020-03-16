9769ac9420f7611c79c412b45299e8ed1030def4
Eugene Prilepin <esp.home@gmail.com>
2020-03-16 19:11:15 +0300

    add `new` methods impl

--- src/ndg.rs
+++ src/ndg.rs
@@ -1,3 +1,5 @@
+use std::convert::AsRef;
+
 use ndarray::{
     NdFloat,
     Dimension,
@@ -8,6 +10,7 @@ use ndarray::{
 };
 
 use almost::AlmostEqual;
+use itertools::Itertools;
 
 use crate::Result;
 
@@ -32,11 +35,33 @@ pub struct NdGridSpline<'a, T: NdFloat, D: Dimension>
     pieces: Vec<usize>,
 
     /// The breaks (data sites for each grid dimension) which have been used for computing spline
-    breaks: &'a [ArrayView1<'a, T>],
+    breaks: Vec<ArrayView1<'a, T>>,
 
     /// N-d array of the tensor-product univariate spline coefficients as
     /// representation of n-d grid spline coefficients
     coeffs: Array<T, D>,
+}
+
+
+impl<'a, T, D> NdGridSpline<'a, T, D>
+    where
+        T: NdFloat + AlmostEqual,
+        D: Dimension
+{
+    ///
+    pub fn new(breaks: Vec<ArrayView1<'a, T>>, coeffs: Array<T, D>) -> Self {
+        let ndim = breaks.len();
+        let pieces: Vec<usize> = breaks.iter().map(|x| x.len() - 1).collect();
+        let order: Vec<usize> = pieces.iter().zip(coeffs.shape().iter()).map(|(p, s)| s / p).collect();
+
+        NdGridSpline {
+            ndim,
+            order,
+            pieces,
+            breaks,
+            coeffs,
+        }
+    }
 }
 
 
@@ -45,7 +70,7 @@ pub struct NdGridCubicSmoothingSpline<'a, T, D>
     where T: NdFloat, D: Dimension
 {
     /// X data sites (also breaks)
-    x: &'a [ArrayView1<'a, T>],
+    x: Vec<ArrayView1<'a, T>>,
 
     /// Y data values
     y: ArrayView<'a, T, D>,
@@ -58,4 +83,23 @@ pub struct NdGridCubicSmoothingSpline<'a, T, D>
 
     /// `NdSpline` struct with computed spline
     spline: Option<NdGridSpline<'a, T, D>>
+}
+
+
+impl<'a, T, D> NdGridCubicSmoothingSpline<'a, T, D>
+    where T: NdFloat + AlmostEqual + Default,
+          D: Dimension
+{
+    pub fn new<X, Y>(x: &'a [X], y: Y) -> Self
+        where X: AsArray<'a, T> + AsRef<[T]>,
+              Y: AsArray<'a, T, D>
+    {
+        NdGridCubicSmoothingSpline {
+            x: x.iter().map_into().collect(),
+            y: y.into(),
+            weights: None,
+            smooth: None,
+            spline: None,
+        }
+    }
 }
