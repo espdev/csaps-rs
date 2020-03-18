932fbc001f3d6eb039f656b217147b67c37547ef
Eugene Prilepin <esp.home@gmail.com>
2020-03-18 16:32:25 +0300

    update test for ndg make

--- tests/ndg_make.rs
+++ tests/ndg_make.rs
@@ -1,4 +1,5 @@
-use ndarray::array;
+use ndarray::{array, Array1};
+use approx::assert_abs_diff_eq;
 
 use csaps::GridCubicSmoothingSpline;
 
@@ -19,5 +20,19 @@ fn test_make_surface() {
     let s = GridCubicSmoothingSpline::new(&x, &y)
         .make().unwrap();
 
-    assert!(s.spline().is_some())
+    let coeffs_expected = array![
+        [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+        [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+        [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+        [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+        [0., 0., 0., 0., 0., 0., 0., 0., 0., 4., 4., 4.],
+        [0., 0., 0., 0., 0., 0., 0., 0., 0., 4., 4., 4.],
+        [0., 0., 0., 0., 0., 0., 1., 1., 1., 1., 2., 3.],
+        [0., 0., 0., 0., 0., 0., 1., 1., 1., 5., 6., 7.],
+     ];
+
+    let smooth: Array1<f64> = s.smooth().iter().map(|v| v.unwrap()).collect();
+
+    assert_abs_diff_eq!(smooth, array![0.8999999999999999, 0.8999999999999999]);
+    assert_abs_diff_eq!(s.spline().unwrap().coeffs(), coeffs_expected)
 }
