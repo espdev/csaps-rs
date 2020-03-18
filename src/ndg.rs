33c1b28b2deba080888d0e054f9b45050166ba69
Eugene Prilepin <esp.home@gmail.com>
2020-03-18 04:30:36 +0300

    remove unused imports

--- src/ndg.rs
+++ src/ndg.rs
@@ -2,8 +2,6 @@ mod validate;
 mod make;
 mod evaluate;
 
-use std::convert::AsRef;
-
 use ndarray::{
     NdFloat,
     Dimension,
@@ -14,7 +12,6 @@ use ndarray::{
 };
 
 use almost::AlmostEqual;
-use itertools::Itertools;
 
 use crate::Result;
 
