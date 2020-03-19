00affceca6b60ae768094e4f1d946ddddf8c96bd
Eugene Prilepin <esp.home@gmail.com>
2020-03-19 16:56:45 +0300

    add vector tests for ndg

--- tests/ndg_evaluate.rs
+++ tests/ndg_evaluate.rs
@@ -5,10 +5,43 @@ use csaps::GridCubicSmoothingSpline;
 
 
 #[test]
-fn test_make_surface() {
+fn test_make_vector_1() {
+    let x0 = array![1., 2., 3., 4.];
+    let x = vec![x0.view()];
+
+    let y = array![1., 2., 3., 4.];
+
+    let yi = GridCubicSmoothingSpline::new(&x, &y)
+        .make().unwrap()
+        .evaluate(&x).unwrap();
+
+    assert_abs_diff_eq!(yi, y);
+}
+
+
+#[test]
+fn test_make_vector_2() {
+    let x0 = array![1., 2., 3., 4.];
+    let x = vec![x0.view()];
+
+    let xi0 = array![1., 1.5, 2., 2.5, 3., 3.5, 4.];
+    let xi = vec![xi0.view()];
+
+    let y = array![1., 2., 3., 4.];
+    let yi_expected = array![1., 1.5, 2., 2.5, 3., 3.5, 4.];
+
+    let yi = GridCubicSmoothingSpline::new(&x, &y)
+        .make().unwrap()
+        .evaluate(&xi).unwrap();
+
+    assert_abs_diff_eq!(yi, yi_expected);
+}
+
+
+#[test]
+fn test_make_surface_1() {
     let x0 = array![1., 2., 3.];
     let x1 = array![1., 2., 3., 4.];
-
     let x = vec![x0.view(), x1.view()];
 
     let y = array![
@@ -26,7 +59,7 @@ fn test_make_surface() {
 
 
 #[test]
-fn test_make_surface_1() {
+fn test_make_surface_2() {
     let x0 = array![1., 2., 3.];
     let x1 = array![1., 2., 3., 4.];
     let x = vec![x0.view(), x1.view()];
