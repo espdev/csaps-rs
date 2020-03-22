2abbda72d9a19ccf8c84c25941a3f1bc7acc381e
Eugene Prilepin <esp.home@gmail.com>
2020-03-22 22:08:11 +0300

    refactoring

--- src/umv/make.rs
+++ src/umv/make.rs
@@ -1,4 +1,4 @@
-use ndarray::{NdFloat, Dimension, Array1, Axis, s, stack, Array2};
+use ndarray::{NdFloat, Dimension, Axis, Array1, Array2, s, stack};
 use sprs::binop::scalar_mul_mat as sprs_mul_s;
 use almost::AlmostEqual;
 
@@ -51,21 +51,21 @@ impl<'a, T, D> CubicSmoothingSpline<'a, T, D>
             return Ok(())
         }
 
-        // General smoothing spline computing for NxM data (3 and more data points)
+        // General computing cubic smoothing spline for NxM data (3 and more data points)
         let ones = |n| Array1::<T>::ones((n, ));
 
-        let qt = {
-            let odx = ones(pcount - 1) / &dx;
-            let odx_head = odx.slice(s![..-1]).insert_axis(Axis(0)).into_owned();
-            let odx_tail = odx.slice(s![1..]).insert_axis(Axis(0)).into_owned();
-            drop(odx);
-            let odx_body = -(&odx_tail + &odx_head);
-            let diags_qt = stack![Axis(0), odx_head, odx_body, odx_tail];
-
-            sprsext::diags(diags_qt, &[0, 1, 2], (pcount - 2, pcount))
-        };
-
         let qtwq = {
+            let qt = {
+                let odx = ones(pcount - 1) / &dx;
+                let odx_head = odx.slice(s![..-1]).insert_axis(Axis(0)).into_owned();
+                let odx_tail = odx.slice(s![1..]).insert_axis(Axis(0)).into_owned();
+                drop(odx);
+                let odx_body = -(&odx_tail + &odx_head);
+                let diags_qt = stack![Axis(0), odx_head, odx_body, odx_tail];
+
+                sprsext::diags(diags_qt, &[0, 1, 2], (pcount - 2, pcount))
+            };
+
             let diags_sqrw = (ones(pcount) / weights.mapv(T::sqrt)).insert_axis(Axis(0));
             let sqrw = sprsext::diags(diags_sqrw, &[0], (pcount, pcount));
             let qtw = &qt * &sqrw;
@@ -90,14 +90,14 @@ impl<'a, T, D> CubicSmoothingSpline<'a, T, D>
             one / (one + trace(&r) / (six * trace(&qtwq)))
         };
 
-        let p = self.smooth.unwrap_or_else(auto_smooth);
-        let p1 = six * (one - p);
+        let smooth = self.smooth.unwrap_or_else(auto_smooth);
+        let s1 = six * (one - smooth);
 
-        // # Solve linear system Ax = b for the 2nd derivatives
-        let u = {
+        // Solve linear system Ax = b for the 2nd derivatives
+        let usol = {
             let a = {
-                let a1 = sprs_mul_s(&qtwq, p1);
-                let a2 = sprs_mul_s(&r, p);
+                let a1 = sprs_mul_s(&qtwq, s1);
+                let a2 = sprs_mul_s(&r, smooth);
                 drop(qtwq);
                 drop(r);
 
@@ -111,53 +111,44 @@ impl<'a, T, D> CubicSmoothingSpline<'a, T, D>
         };
 
         // Compute and stack spline coefficients
-
-        let vpad = |a: &Array2<T>| -> Array2<T> {
-            let p = Array2::<T>::zeros((1, a.shape()[1]));
-            stack(Axis(0), &[p.view(), a.view(), p.view()]).unwrap()
-        };
-
-        let dx = dx.insert_axis(Axis(1));
-
-        let (d1, d2) = {
-            let u_pad = vpad(&u);
-            let d1 = diff(&u_pad, Some(Axis(0))) / &dx;
-            let d1_pad = vpad(&d1);
-            let d2 = diff(&d1_pad, Some(Axis(0)));
-
-            (d1, d2)
-        };
-
-        let yi = {
-            let diags_w = (ones(pcount) / weights).insert_axis(Axis(0));
-            let w = sprsext::diags(diags_w, &[0], (pcount, pcount));
-            let wd2 = &sprs_mul_s(&w, p1) * &d2;
-
-            drop(d1);
-            drop(d2);
-
-            &y.t() - &wd2
-        };
-
-        let c3 = vpad(&(u * p));
-        let c3_head = c3.slice(s![..-1, ..]);
-        let c3_tail = c3.slice(s![1.., ..]);
-
-        let p3 = {
-            let dyi = diff(&yi, Some(Axis(0)));
-            &dyi / &dx - ((&c3_head * two + c3_tail) * &dx)
-        };
-
         let coeffs = {
+            let vpad = |arr: &Array2<T>| -> Array2<T> {
+                let pad = Array2::<T>::zeros((1, arr.shape()[1]));
+                stack(Axis(0), &[pad.view(), arr.view(), pad.view()]).unwrap()
+            };
+
+            let dx = dx.insert_axis(Axis(1));
+
+            let yi = {
+                let d1 = diff(&vpad(&usol), Some(Axis(0))) / &dx;
+                let d2 = diff(&vpad(&d1), Some(Axis(0)));
+
+                let diags_w = (ones(pcount) / weights).insert_axis(Axis(0));
+                let w = sprsext::diags(diags_w, &[0], (pcount, pcount));
+                let wd2 = &sprs_mul_s(&w, s1) * &d2;
+
+                drop(d1);
+                drop(d2);
+
+                &y.t() - &wd2
+            };
+
+            let c3 = vpad(&(usol * smooth));
+            let c3_head = c3.slice(s![..-1, ..]);
+            let c3_tail = c3.slice(s![1.., ..]);
+
             let p1 = diff(&c3, Some(Axis(0))) / &dx;
-            drop(dx);
             let p2 = &c3_head * three;
+            let p3 = diff(&yi, Some(Axis(0))) / &dx - (&c3_head * two + c3_tail) * dx;
             let p4 = yi.slice(s![..-1, ..]);
+
+            drop(c3_head);
+            drop(c3);
 
             stack![Axis(0), p1, p2, p3, p4].t().to_owned()
         };
 
-        self.smooth = Some(p);
+        self.smooth = Some(smooth);
         self.spline = Some(NdSpline::new(breaks, coeffs));
 
         Ok(())
